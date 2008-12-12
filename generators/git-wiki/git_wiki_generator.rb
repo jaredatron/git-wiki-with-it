fd899a41e4de67e7fe7540b2e55a0dc2303b9cac
Jared Grippe <jared@deadlyicon.com>
2008-12-11 20:37:27 -0800

    adding git-wiki generators

---
+++ generators/git-wiki/git_wiki_generator.rb
@@ -0,0 +1,8 @@
+class Git-wiki-with-itGenerator < Rails::Generator::NamedBase
+  def manifest
+    record do |m|
+      # m.directory "lib"
+      # m.template 'README', "README"
+    end
+  end
+end
