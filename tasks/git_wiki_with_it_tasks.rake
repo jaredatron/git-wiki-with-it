f2aba05e252e5a91520a7e200eab3cf1a04f2db6
Jared Grippe <jared@deadlyicon.com>
2008-12-11 20:01:19 -0800

    added base plugin structure

---
+++ tasks/git_wiki_with_it_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :git_wiki_with_it do
+#   # Task goes here
+# end
---
+++ test/git_wiki_with_it_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class Git-wiki-with-itTest < ActiveSupport::TestCase
+  # Replace this with your real tests.
+  test "the truth" do
+    assert true
+  end
+end
